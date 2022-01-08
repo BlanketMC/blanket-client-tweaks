62d3fba47c47f0754732a1297eb3d558b045396d
KosmX <kosmx.mc@gmail.com>
2022-01-08 19:45:03 +0100

    some refractor

--- src/main/java/io/github/blanketmc/blanket/config/BlanketConfigScreenProvider.java
+++ src/main/java/io/github/blanketmc/blanket/config/BlanketConfigScreenProvider.java
@@ -2,14 +2,89 @@ package io.github.blanketmc.blanket.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import io.github.blanketmc.blanket.Config;
 import io.github.blanketmc.blanket.FabricModInitializer;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.TranslatableText;
+import org.apache.logging.log4j.Level;
+
 
 /**
  * Creates a cloth-config screen for mod configs
  */
+@SuppressWarnings("unchecked")
 public class BlanketConfigScreenProvider implements ModMenuApi {
+    private static final Config defaults = new Config();
+
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> ConfigHelper.getScreen(parent, FabricModInitializer.config);
+        return parent -> getScreen(parent, FabricModInitializer.config);
     }
+
+    public static Screen getScreen(Screen parent, Config config) {
+        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(new TranslatableText("blanket-client-tweaks.config.title"));
+        builder.setSavingRunnable(() -> ConfigHelper.saveConfig(config));
+
+        //Config entry category
+        ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("blanket-client-tweaks.config.general")); //we can ignore the title, until we have more categories
+        addEntriesToCategory(general, builder.entryBuilder(), config);
+
+        return builder.build();
+    }
+
+    private static void addEntriesToCategory(ConfigCategory category, ConfigEntryBuilder entryBuilder, Config config){
+        ConfigHelper.iterateOnConfig((field, configEntry) -> {
+            var type = field.getType();
+            if (type.equals(Boolean.TYPE)) {
+
+                var entry = entryBuilder.startBooleanToggle(ConfigHelper.getTextComponent(configEntry.displayName(), field.getName()), field.getBoolean(config));
+
+                //saveConsumer
+                entry.setSaveConsumer(aBoolean -> {
+                    try {
+                        field.set(config, aBoolean);
+                    } catch(IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                //default value using mirror class
+                boolean defVal = field.getBoolean(defaults);
+                entry.setDefaultValue(defVal);
+
+                //Description
+                if (!configEntry.description().equals("")) {
+                    entry.setTooltip(ConfigHelper.getTextComponent(configEntry.description(), null));
+                }
+
+                category.addEntry(entry.build());
+
+            } else if (type.isEnum()) {
+
+                var clazz = (Class<Enum<?>>) type;
+
+                Object obj = field.get(config);
+                var entry = entryBuilder.startEnumSelector(ConfigHelper.getTextComponent(configEntry.displayName(), field.getName()), clazz, clazz.cast(obj));
+
+                //default value using mirror class
+                Object defVal = field.get(defaults);
+                entry.setDefaultValue(clazz.cast(defVal));
+
+                //Description
+                if (!configEntry.description().equals("")) {
+                    entry.setTooltip(ConfigHelper.getTextComponent(configEntry.description(), null));
+                }
+
+                category.addEntry(entry.build());
+
+            } else {
+                FabricModInitializer.log(Level.ERROR, "Config: " + field.getName() + " can not be displayed: Unknown type", true);
+            }
+        });
+    }
+
 }
--- src/main/java/io/github/blanketmc/blanket/config/ConfigHelper.java
+++ src/main/java/io/github/blanketmc/blanket/config/ConfigHelper.java
@@ -1,88 +1,22 @@
 package io.github.blanketmc.blanket.config;
 
 import io.github.blanketmc.blanket.Config;
-import io.github.blanketmc.blanket.FabricModInitializer;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import org.apache.logging.log4j.Level;
 
 import java.lang.reflect.Field;
 
-@SuppressWarnings("unchecked")
 public final class ConfigHelper {
 
-    private static final Config defaults = new Config();
-
-    public static Screen getScreen(Screen parent, Config config) {
-        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(new TranslatableText("blanket-client-tweaks.config.title"));
-        builder.setSavingRunnable(() -> saveConfig(config));
-
-        //Config entry category
-        ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("blanket-client-tweaks.config.general")); //we can ignore the title, until we have more categories
-        addEntriesToCategory(general, builder.entryBuilder(), config);
-
-        return builder.build();
-    }
-
-    private static void addEntriesToCategory(ConfigCategory category, ConfigEntryBuilder entryBuilder, Config config) {
-
-        Field[] fields = config.getClass().getFields();
+    public static void iterateOnConfig(ConfigIterator iterator){
+        Field[] fields = Config.class.getFields();
         for (var field : fields) {
             try {
                 ConfigEntry fieldInfo = field.getAnnotation(ConfigEntry.class);
                 if (fieldInfo == null) continue; //this is not a config entry
 
-                var type = field.getType();
-                if (type.equals(Boolean.TYPE)) {
-
-                    var entry = entryBuilder.startBooleanToggle(getTextComponent(fieldInfo.displayName(), field.getName()), field.getBoolean(config));
-
-                    //saveConsumer
-                    entry.setSaveConsumer(aBoolean -> {
-                        try {
-                            field.set(config, aBoolean);
-                        } catch(IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
-                    });
-
-                    //default value using mirror class
-                    boolean defVal = field.getBoolean(defaults);
-                    entry.setDefaultValue(defVal);
-
-                    //Description
-                    if (!fieldInfo.description().equals("")) {
-                        entry.setTooltip(getTextComponent(fieldInfo.description(), null));
-                    }
-
-                    category.addEntry(entry.build());
-
-                } else if (type.isEnum()) {
-
-                    var clazz = (Class<Enum<?>>) type;
-
-                    Object obj = field.get(config);
-                    var entry = entryBuilder.startEnumSelector(getTextComponent(fieldInfo.displayName(), field.getName()), clazz, clazz.cast(obj));
-
-                    //default value using mirror class
-                    Object defVal = field.get(defaults);
-                    entry.setDefaultValue(clazz.cast(defVal));
-
-                    //Description
-                    if (!fieldInfo.description().equals("")) {
-                        entry.setTooltip(getTextComponent(fieldInfo.description(), null));
-                    }
-
-                    category.addEntry(entry.build());
-
-                } else {
-                    FabricModInitializer.log(Level.ERROR, "Config: " + field.getName() + " can not be displayed: Unknown type", true);
-                }
+                iterator.acceptConfigEntry(field, fieldInfo);
             } catch(IllegalAccessException ignored) {}
         }
     }
@@ -106,5 +40,9 @@ public final class ConfigHelper {
     public static Config loadConfig() {
         //TODO
         return new Config();
+    }
+
+    public static interface ConfigIterator{
+        public void acceptConfigEntry(Field field, ConfigEntry configEntry) throws IllegalAccessException;
     }
 }
