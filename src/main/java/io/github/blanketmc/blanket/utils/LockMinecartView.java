389c7ee7d0635140c0b4906400777f44ec6da288
KosmX <kosmx.mc@gmail.com>
2022-01-21 20:13:11 +0100

    auto format to match our style

--- src/main/java/io/github/blanketmc/blanket/utils/LockMinecartView.java
+++ src/main/java/io/github/blanketmc/blanket/utils/LockMinecartView.java
@@ -37,7 +37,7 @@ public class LockMinecartView {
 
     //-----------------CORE METHOD-----------------------
 
-    public static void update(MinecartEntity minecart){
+    public static void update(MinecartEntity minecart) {
         lastYaw = yaw;
         boolean update = setMinecartDirection(minecart);
         if (tickAfterLastFollow++ > LockMinecartView.threshold) lastYaw = yaw;
@@ -47,38 +47,38 @@ public class LockMinecartView {
         difference = normalize(yaw - lastYaw);
     }
 
-    public static boolean setMinecartDirection(MinecartEntity minecart){
+    public static boolean setMinecartDirection(MinecartEntity minecart) {
         boolean update = false;
         float yawF = rawYaw;
         boolean correction = false;
         boolean successUpdate = updateSmartCorrection(minecart);
-        if (minecart.getVelocity().lengthSquared()>0.000002f) {
+        if (minecart.getVelocity().lengthSquared() > 0.000002f) {
             if (tickAfterPistonRail != LockMinecartView.threshold) tickAfterPistonRail++;
             if (pistorRailTick != 0) pistorRailTick--;
             yawF = sphericalFromVec3d(minecart.getVelocity());
             update = true;
             correction = true;
-        } else if(minecart.getVelocity().lengthSquared() == 0f && successUpdate && posVelocity.lengthSquared() > 0.02f){
-            if(pistorRailTick != LockMinecartView.threshold) pistorRailTick++;
+        } else if (minecart.getVelocity().lengthSquared() == 0f && successUpdate && posVelocity.lengthSquared() > 0.02f) {
+            if (pistorRailTick != LockMinecartView.threshold) pistorRailTick++;
             else tickAfterPistonRail = 0;
             yawF = getEighthDirection(sphericalFromVec3d(posVelocity));
             update = true;
         } else {
-            if(pistorRailTick != 0) pistorRailTick--;
+            if (pistorRailTick != 0) pistorRailTick--;
         }
         rawLastYaw = rawYaw;
         rawYaw = yawF;
-        if(correction) checkSmartCorrection(successUpdate);
+        if (correction) checkSmartCorrection(successUpdate);
         setMinecartDirection(yawF);
         return update;
     }
     //-------------methods-----------------------------
 
-    private static float sphericalFromVec3d(Vec3d vec3d){
-        return (float)(MathHelper.atan2(-vec3d.x, vec3d.z) * 57.2957763671875D);
+    private static float sphericalFromVec3d(Vec3d vec3d) {
+        return (float) (MathHelper.atan2(-vec3d.x, vec3d.z) * 57.2957763671875D);
     }
 
-    public static void onStartRiding(){
+    public static void onStartRiding() {
         lastCoord = null;
         tickAfterLastFollow = 100;
         lastVelocity = Vec3d.ZERO;
@@ -87,17 +87,17 @@ public class LockMinecartView {
         pistorRailTick = 0;
     }
 
-    private static void setMinecartDirection(float yawF){
+    private static void setMinecartDirection(float yawF) {
         if (LockMinecartView.smoothMode) {
             if (!LockMinecartView.rollerCoasterMode && tickAfterLastFollow > LockMinecartView.threshold) {
                 yaw = yawF;
             } else if (doCorrection) {
                 yaw = normalize(yaw + 180f);
             }
-            if (Math.abs(yawF - yaw) < 180f){
-                yaw = yaw/2 + yawF/2;
+            if (Math.abs(yawF - yaw) < 180f) {
+                yaw = yaw / 2 + yawF / 2;
             } else {
-                float tmp = yaw/2 + yawF/2;
+                float tmp = yaw / 2 + yawF / 2;
                 yaw = (tmp >= 0) ? tmp - 180f : tmp + 180f;
             }
         } else {
@@ -106,14 +106,14 @@ public class LockMinecartView {
     }
 
 
-    private static void checkSmartCorrection(boolean successUpdate){
+    private static void checkSmartCorrection(boolean successUpdate) {
         boolean correction = false;
-        if(LockMinecartView.smartMode){
+        if (LockMinecartView.smartMode) {
             float ang = 60f;
-            if (tickAfterPistonRail == LockMinecartView.threshold && floatCircleDistance(rawLastYaw, rawYaw, 360) > 180f-ang && floatCircleDistance(rawLastYaw, rawYaw, 360) <180+ang) {
+            if (tickAfterPistonRail == LockMinecartView.threshold && floatCircleDistance(rawLastYaw, rawYaw, 360) > 180f - ang && floatCircleDistance(rawLastYaw, rawYaw, 360) < 180 + ang) {
                 correction = true;
                 /*-------------------Explain, what does the following complicated code------------------------
-                 *The Smart correction's aim is to make difference between a U-turn and a collision, what is'n an easy task
+                 *The Smart correction's aim is to make difference between a U-turn and a collision, what isn't an easy task
                  * The speed vector always rotate in 180* so I need data from somewhere else:Position->real speed(with a little delay)
                  * I observed 2 things:
                  * 1:On collision, the speed decreases (gotVelocity)
@@ -138,36 +138,36 @@ public class LockMinecartView {
         doCorrection = correction;
     }
 
-    private static boolean updateSmartCorrection(MinecartEntity minecart){
+    private static boolean updateSmartCorrection(MinecartEntity minecart) {
         boolean success = lastCoord != null;
         Vec3d pos = minecart.getPos();
-        if(success) {
+        if (success) {
             posVelocity = new Vec3d(pos.x - lastCoord.x, 0, pos.z - lastCoord.z);
             lastVelocity = (gotVelocity == null) ? new Vec3d(0, 0, 0) : gotVelocity;
             gotVelocity = new Vec3d(minecart.getVelocity().getX(), 0, minecart.getVelocity().getZ());
-            if( gotVelocity.length() != 0 && lastVelocity.length()/gotVelocity.length() > 2.4d) lastSlowdown = 0;
+            if (gotVelocity.length() != 0 && lastVelocity.length() / gotVelocity.length() > 2.4d) lastSlowdown = 0;
             ++lastSlowdown;
         }
         lastCoord = pos;
         return success;
     }
 
-    public static float calcYaw(float entityYaw){
+    public static float calcYaw(float entityYaw) {
         return (LockMinecartView.rollerCoasterMode) ? (entityYaw + normalize(yaw - entityYaw)) : (entityYaw + difference);
     }
 
 
-    private static float normalize(Float f){
+    private static float normalize(Float f) {
         return (Math.abs(f) > 180) ? (f < 0) ? f + 360f : f - 360f : f;
     }
 
-    private static float getEighthDirection(Float f){
-        if(floatCircleDistance(f%90 , 0, 90) < 20) return ((float)Math.round(f/90))*90;
+    private static float getEighthDirection(Float f) {
+        if (floatCircleDistance(f % 90, 0, 90) < 20) return ((float) Math.round(f / 90)) * 90;
         return f;
     }
 
-    private static float floatCircleDistance(float i1, float i2, float size){
-        float dist1 = Math.abs(i1-i2);
+    private static float floatCircleDistance(float i1, float i2, float size) {
+        float dist1 = Math.abs(i1 - i2);
         return Math.min(dist1, Math.abs(dist1 - size));
     }
 }
